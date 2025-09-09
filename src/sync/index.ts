0cd642cc14914bb64a8969971bf5f96d7a9ca27d
VanshDevChoudhary <vanshdevchoudhary007@gmail.com>
2025-09-09 16:21:00 +0530

    chore: sync module barrel export

---
+++ src/sync/index.ts
@@ -0,0 +1,3 @@
+export { SyncEngine } from './engine';
+export type { SyncListener } from './engine';
+export { incrementVersion, compareVectors, mergeVectors, parseVector, serializeVector } from './versionVector';
