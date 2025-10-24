1591fbc5e4b267a798c9b0213a6c954bdbfe9039
VanshDevChoudhary <vanshdevchoudhary007@gmail.com>
2025-10-24 19:47:00 +0530

    chore: register user profile in realm provider

--- src/db/provider.tsx
+++ src/db/provider.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { RealmProvider as RealmCtxProvider } from '@realm/react';
-import { Habit, HabitCompletion, WorkoutSet, Exercise, Workout, HealthSnapshot, SyncLog } from './schema';
+import { Habit, HabitCompletion, WorkoutSet, Exercise, Workout, HealthSnapshot, UserProfile, SyncLog } from './schema';
 
-const schema = [Habit, HabitCompletion, WorkoutSet, Exercise, Workout, HealthSnapshot, SyncLog];
+const schema = [Habit, HabitCompletion, WorkoutSet, Exercise, Workout, HealthSnapshot, UserProfile, SyncLog];
 
 export default function RealmProvider({ children }: { children: React.ReactNode }) {
   return (
-    <RealmCtxProvider schema={schema} schemaVersion={1}>
+    <RealmCtxProvider schema={schema} schemaVersion={2} deleteRealmIfMigrationNeeded>
       {children}
     </RealmCtxProvider>
   );
